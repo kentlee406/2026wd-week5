cbd18eb905ad65c87e947ffb4c93486c3452a3db
kentlee406 <157895655+kentlee406@users.noreply.github.com>
2026-08-08 07:27:14 +0800

    20260808最終上傳

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-import './assets/scss/all.scss';
+import "/assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-console.log('Hello world');
+console.log("Hello world");
